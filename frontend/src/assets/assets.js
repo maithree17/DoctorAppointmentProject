d25ba2055e46c9b720886be3f7dabdac48eb54f1
maithree17 <maithreemuliya@gmail.com>
2026-02-01 10:41:04 +0530

    Update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import appointment_img from './appointment_img.png'
+import welcome_appointment from './welcome_appointment.png'
 import header_img from './header_img.png'
 import group_profiles from './group_profiles.png'
 import profile_pic from './profile_pic.png'
@@ -39,7 +39,7 @@ import Pediatricians from './Pediatricians.svg'
 
 
 export const assets = {
-    appointment_img,
+    welcome_appointment,
     header_img,
     group_profiles,
     logo,
